fb97f1c9fe289b0f7ea6aa57a9de1b60165c7e98
Jason Sherman <jsn.sherman@gmail.com>
2018-11-06 08:44:19 -0600

    add a script for building and deploying everything because I'm a lazy typist.

---
+++ bin/deploy_all.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source bin/activate
+
+kubectl apply -f secrets/
+
+cd processing
+docker build -t envirodgi/processing .
+cd ../ui
+# While building ui, you may find that the yarn install steps error out. Yarn seems to have some sensitivity to the network conditions within kubernetes.  I found that doing a simple try loop got me through the build after a while.
+time until docker build -t envirodgi/ui .; do echo "ERROR: build failed; retrying"; done
+cd ../db
+docker build --target rails-server -t envirodgi/db-rails-server .
+docker build --target import-worker -t envirodgi/db-import-worker .
+cd ..
+
+kubectl apply -f templates/
