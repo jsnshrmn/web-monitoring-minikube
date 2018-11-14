57ae4c06974f4ad96ed2026b57bf0895404190e5
Jason Sherman <jsn.sherman@gmail.com>
2018-11-14 08:53:27 -0600

    Capture work on convenience scripts.

--- bin/db_setup.sh
+++ bin/db_setup.sh
@@ -2,7 +2,11 @@
 
 source bin/activate
 
-db_pod=$(kubectl get pods --selector=app=api --output=jsonpath={.items..metadata.name})
+app=api
+pod=$(kubectl get pods --selector=app=${api} --output=jsonpath={.items..metadata.name})
+
 kubectl exec ${db_pod} bundle exec rake db:create
+
 kubectl exec ${db_pod} bundle exec rake db:schema:load
-echo "User.create(email: 'seed-admin@example.com', password: 'PASSWORD', admin: true, confirmed_at: Time.now)" | kubectl exec -it ${db_pod} bundle exec rails console
+
+echo "User.create(email: 'seed-admin@example.com', password: 'PASSWORD', admin: true, confirmed_at: Time.now)" | kubectl exec -it ${pod} bundle exec rails console
