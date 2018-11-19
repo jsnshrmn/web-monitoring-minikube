f43a961135f1f565be5761ef13997778dc9a9f22
Jason Sherman <jsn.sherman@gmail.com>
2018-11-19 14:45:59 -0600

    add annotation user

--- bin/db_setup.sh
+++ bin/db_setup.sh
@@ -14,6 +14,7 @@ done
 
 pod=$(kubectl get pods --selector=app=api --output=jsonpath={.items..metadata.name})
 
-kubectl exec ${pod} bundle exec rake db:create && \
-kubectl exec ${pod} bundle exec rake db:schema:load && \
+kubectl exec ${pod} bundle exec rake db:create || return 1
+kubectl exec ${pod} bundle exec rake db:schema:load || return 1
 echo "User.create(email: 'seed-admin@example.com', password: 'PASSWORD', admin: true, confirmed_at: Time.now)" | kubectl exec -it ${pod} bundle exec rails console
+echo "User.create(email: 'annotation-bot@example.com', password: 'PASSWORD', admin: true, confirmed_at: Time.now)" | kubectl exec -it ${pod} bundle exec rails console
